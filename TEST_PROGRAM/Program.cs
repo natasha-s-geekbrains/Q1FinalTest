fb5e2cc84814ebc5ef171812537d8c7eff39a94c
natasha_s <natasl1012@gmail.com>
2023-09-05 21:12:00 +0300

    final program file

--- TEST_PROGRAM/Program.cs
+++ TEST_PROGRAM/Program.cs
@@ -9,8 +9,11 @@
 //[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 //[“Russia”, “Denmark”, “Kazan”] → []
 
-string stringsSet = GetStringsSet("Введите массив строк через запятую: ");
-Console.WriteLine(stringsSet);
+string stringsSet = GetStringsSet("Введите массив строк через запятую с пробелом: ");
+Console.Write($"[{stringsSet}] -> ");
+string[] firstArray = GetUserArray(stringsSet);
+string[] finalArray = GetResultArray(firstArray);
+Console.Write($"[{String.Join(", ", finalArray)}]");
 
 
 string GetStringsSet(string message)
@@ -18,4 +21,27 @@ string GetStringsSet(string message)
     Console.Write(message);
     return Console.ReadLine()!;
 }
+
+string[] GetUserArray(string userText)
+{
+    string[] userArray = userText.Split(", ");
+    return userArray;
+}
+
+string[] GetResultArray(string[] arr1)
+{
+    string[] arr2 = Array.Empty<string>();
+    int newIndx = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            Array.Resize(ref arr2, newIndx+1);
+            arr2[newIndx] = arr1[i];
+            newIndx++;
+        }
+    }
+    return arr2;
+}
+
 
